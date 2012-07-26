452064c1f635bdbad96d89df4d162f30b0afe58d
kishii106 <kishii106@yahoo.co.jp>
2012-07-27 02:07:56 +0900

    commit

--- src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
+++ src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
@@ -1,7 +1,14 @@
 package jp.gr.uchiwa.blackout.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class BukkenListActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+	}
 
 }
