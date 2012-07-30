caffa64e8d5a18698411578bcd71de2c3198e90f
kishii106 <kishii106@yahoo.co.jp>
2012-07-30 23:42:09 +0900

    commit

--- src/jp/gr/uchiwa/blackout/activity/BukkenEditActivity.java
+++ src/jp/gr/uchiwa/blackout/activity/BukkenEditActivity.java
@@ -1,7 +1,43 @@
 package jp.gr.uchiwa.blackout.activity;
 
+import jp.gr.uchiwa.brackout.R;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class BukkenEditActivity extends Activity {
 
+	private EditText bukkenName;
+	private EditText subGroup;
+	private EditText contactAddress;
+	private EditText contactPerson;
+	private Button   bukkenEntry;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		findView();
+		addEventHandler();
+		bindData();
+	}
+
+	private void findView() {
+		bukkenName     = (EditText) findViewById(R.id.bukkenName);
+		subGroup       = (EditText) findViewById(R.id.subGroup);
+		contactAddress = (EditText) findViewById(R.id.contactAddress);
+		contactPerson  = (EditText) findViewById(R.id.contactPerson);
+		bukkenEntry    = (Button) findViewById(R.id.bukkenUpdate);
+	}
+
+	private void addEventHandler() {
+		bukkenEntry.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			}
+		});
+	}
+
+	private void bindData() {
+	}
 }
--- src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
+++ src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
@@ -1,14 +1,52 @@
 package jp.gr.uchiwa.blackout.activity;
 
+import jp.gr.uchiwa.brackout.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class BukkenListActivity extends Activity {
 
+	private Button   moveToBukkenEdit;
+	private Button   moveToBlackoutSchedule;
+	private ListView bukkenList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
+		findView();
+		customizeView();
+		addEventHandler();
+		bindData();
 	}
 
+	private void findView() {
+		moveToBukkenEdit       = (Button) findViewById(R.id.moveToBukkenEdit);
+		moveToBlackoutSchedule = (Button) findViewById(R.id.moveToBlackoutSchedule);
+		bukkenList             = (ListView) findViewById(R.id.bukkenList);
+	}
+
+	private void customizeView() {
+		TextView bukkenListHeader = new TextView(this);
+		bukkenListHeader.setText("物件名／サブグループ");
+		bukkenList.addHeaderView(bukkenListHeader);
+	}
+
+	private void addEventHandler() {
+		moveToBukkenEdit.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			}
+		});
+		moveToBlackoutSchedule.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+			}
+		});
+	}
+
+	private void bindData() {
+		
+	}
 }
