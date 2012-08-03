7febd9c6501b3efeea36286eb409106b1eaee736
kishii106 <kishii106@yahoo.co.jp>
2012-08-03 21:10:02 +0900

    BukkenListActivityとBukkenEditActivityを追加。

--- src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
+++ src/jp/gr/uchiwa/blackout/activity/BukkenListActivity.java
@@ -2,6 +2,7 @@ package jp.gr.uchiwa.blackout.activity;
 
 import jp.gr.uchiwa.blackout.R;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,8 @@ public class BukkenListActivity extends Activity {
 	private void addEventHandler() {
 		moveToBukkenEdit.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
+				Intent intent = new Intent(BukkenListActivity.this, BukkenEditActivity.class);
+				startActivity(intent);
 			}
 		});
 		moveToBlackoutSchedule.setOnClickListener(new View.OnClickListener() {
